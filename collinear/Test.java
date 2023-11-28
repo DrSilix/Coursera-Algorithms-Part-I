e6a29e6c22ebae84229e5dc18c7166e48c13664f
Alex Hackl <alexhackl@live.com>
2023-11-27 21:37:13 -0600

    Loaded project files collinear wrote the point compareTo method wrote the slopeTo method

---
+++ collinear/Test.java
@@ -0,0 +1,16 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Test {
+    public static void main(String[] args) {
+        Point a = new Point(1, 1);
+        Point b = new Point(2, 2);
+
+        StdOut.println(a.slopeTo(b));
+    }
+}
